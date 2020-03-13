408f7920f588772676b4a2f984435336f3950b8b
George Pickering <9803299+bigpick@users.noreply.github.com>
2020-03-13 15:12:41 -0400

    Group category colors by text value

---
+++ _posts/2020-03-13-dracula-origin.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "The origin of my favorite theme"
+categories: [link post]
+link: https://draculatheme.com/about
+share: true
+---
+
+> I always believed in the cost of context switching. I know how it feels when you're "in the zone", then suddenly, you get distracted and lose focus. It shouldn't be that way, so I decided to create my own color scheme, and my mission was to make it available everywhere.
+
+
--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-});
-
-$(document).ready(function() {
   var colors = ["#6272a4", "#8be9fd", "#50fa7b", "#ffb86c", "#ff79c6", "#bd93f9", "#ff5555", "#f1fa8c"],
       tags = $("a.post-tag");
-
+  let cat_colors = new Map();
   for (var i = 0; i < tags.length; i++) {
-    var selectedColor = colors[Math.floor(Math.random()*colors.length)];
-    $(tags[i]).css('background-color', selectedColor);
+    tag_color = cat_colors.get(tags[i].text);
+    if (tag_color == null) {
+      var selectedColor = colors[Math.floor(Math.random()*colors.length)];
+      $(tags[i]).css('background-color', selectedColor);
+      cat_colors.set(tags[i].text, selectedColor);
+    } else{
+      $(tags[i]).css('background-color', tag_color);
+    }
   }
 });
