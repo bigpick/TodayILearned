303a41fa367c267f8e78e404e25cee037cc0064b
George Pickering <9803299+bigpick@users.noreply.github.com>
2020-04-23 14:45:06 -0400

    Update tag color selection to try not to reuse colors

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  var colors = ["#6272a4", "#8be9fd", "#50fa7b", "#ffb86c", "#ff79c6", "#bd93f9", "#ff5555", "#f1fa8c"],
-      tags = $("a.post-tag");
+  var unused_colors = ["#6272a4", "#8be9fd", "#50fa7b", "#ffb86c", "#ff79c6", "#bd93f9", "#ff5555", "#f1fa8c"],
+      tags = $("a.post-tag"), used_colors = [];
   let cat_colors = new Map();
   for (var i = 0; i < tags.length; i++) {
     tag_color = cat_colors.get(tags[i].text);
     if (tag_color == null) {
-      var selectedColor = colors[Math.floor(Math.random()*colors.length)];
-      $(tags[i]).css('background-color', selectedColor);
-      cat_colors.set(tags[i].text, selectedColor);
+      if (unused_colors.length != 0){
+        var selectedColor = unused_colors[Math.floor(Math.random()*unused_colors.length)];
+        $(tags[i]).css('background-color', selectedColor);
+        cat_colors.set(tags[i].text, selectedColor);
+        var index = unused_colors.indexOf(selectedColor)
+        unused_colors.splice(index, 1)
+        used_colors.push(selectedColor)
+      } else{
+        var selectedColor = used_colors[Math.floor(Math.random()*used_colors.length)];
+        $(tags[i]).css('background-color', selectedColor);
+        cat_colors.set(tags[i].text, selectedColor);
+      }
     } else{
       $(tags[i]).css('background-color', tag_color);
     }
